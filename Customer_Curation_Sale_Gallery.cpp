c92a14622091cd6ca73675c9e184d5921cb164c1
Andy Poon <andypoon923@gemtrifi.com>
2023-10-31 05:49:06 -0700

    added all necessary files and text files

---
+++ Customer_Curation_Sale_Gallery.cpp
@@ -0,0 +1,18 @@
+//
+//  Customer_Curation_Sale_Gallery.cpp
+//  ArtGallerySimplified
+//
+//  implement/define the member functions of the following classes:
+//     1. class Customer; 2. class Curation; 3. class Sale; 4. class ArtGallery
+//
+//created by: [your name]
+//date:  
+
+#include "Customer_Curation_Sale_Gallery.h"
+
+namespace NS_ARTGALLERY{
+   
+ //  implement/define the member functions of the following classes:
+//     1. class Customer; 2. class Curation; 3. class Sale; 4. class ArtGallery
+
+}//end of NS_ARTGALLERY
